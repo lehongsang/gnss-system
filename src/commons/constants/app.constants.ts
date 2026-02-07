4e1be18d2a41ef0d5692df6de4d17bb9ecb2879a
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2026-02-07 11:35:14 +0700

    cleanup(auth): remove unused guards, filters, middlewares and constants

--- src/commons/constants/app.constants.ts
+++ src/commons/constants/app.constants.ts
@@ -1,10 +1,4 @@
-export const BEFORE_HOOK_KEY = Symbol('BEFORE_HOOK');
-export const AFTER_HOOK_KEY = Symbol('AFTER_HOOK');
-export const HOOK_KEY = Symbol('HOOK');
-export const AUTH_INSTANCE_KEY = Symbol('AUTH_INSTANCE');
-export const AUTH_MODULE_OPTIONS_KEY = Symbol('AUTH_MODULE_OPTIONS');
 export const AUTH_IGNORE_ROUTERS = ['messages'];
-export const ROLE_METADATA_KEY = Symbol('ROLE_METADATA_KEY');
 export const DEFAULT_PORT = 3000;
 export const API_GLOBAL_PREFIX = 'api';
 export const APP_NAME = 'Anex';
